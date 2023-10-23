3463d3affa8b121450d9fa7d456e2bc2b60a872f
Joe Dobrow <joedobrow@MacBook-Pro.local>
2023-10-23 05:10:59 -0500

    moving from js to controller

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,35 +1,12 @@
 class GameController < ApplicationController
 
-  DINO_HEALTH = 11
-  DINO_BABY_HEALTH = 4
-  DINO_REPRODUCE_HEALTH = 5;
-  FERN_HEALTH = 10;
-  EGG_HEALTH = 3;
-  FERN_SPAWNRATE = 0.02;
-
   before_action :initialize_game_state, only: [:start]
 
   def start
   end
 
-  def create_unit
-    name = params[:name]
-    x = params[:x].to_i
-    y = params[:y].to_i
-    
-    case name
-    when 'dino'
-      create_dino(x, y)
-    when 'fern'
-      create_fern(x, y)
-    when 'diseased_fern'
-      create_diseased_fern(x, y)
-    when 'egg'
-      create_egg(x, y)
-    else
-      render json: { error: "bad new unit name: #{name}" }, status: 400
-      return
-    end
+  def form
+    render partial 'form'
   end
 
   private
@@ -41,46 +18,86 @@ class GameController < ApplicationController
       diseased_ferns: [],
       eggs: []
     }
+
+    @dino_health = 11
+    @dino_baby_health = 4
+    @dino_reproduce_health = 5
+    @fern_health = 10
+    @egg_health = 3
+    @fern_spawnrate = 0.02
+
+    @board_height = 6
+    @board_width = 9
+
+    @epoch = 0
+
+    @starting_unit_weights = {
+      dino: 1,
+      fern: 2,
+      nil: 7
+    }
   end
 
-  def new_epoch
-    checkEndOfGame();
+  def generate_board
+    probabilities = []
+    probabilities << [:dino] * @starting_unit_weights[:dino]
+    probabilities << [:fern] * @starting_unit_weights[:fern]
+    probabilities << [:nil] * @starting_unit_weights[:nil]
 
-    // update gameState first, ordering matters!
-    moveDinosRandomly();
-    dinosEatFerns();
-    dinosEatDiseasedFerns();
-    hungryDinosDie();
-    hatchEggs();
-    poopEggs();
-    decayFerns()
-    checkForDiseasedFerns();
-    spawnFerns();
-
-    if (epoch % 5 == 0) {
-    adjustFernSpawnRate();
-    }
-
-    clearUnits();
-    drawUnits();
-
-    @epoch = @epoch + 1
+    (0...@board_width).each do |x|
+      (0...@board_height).each do |y|
+        unit = probabilities.sample
+        create_unit(x, y, unit) unless unit.nil?
+      end
+    end
+  end
     
-    render json: { gameState: @game_state }
+
+  def new_epoch
+    check_end_of_game
+
+    # ordering matters!
+    move_dinos_randomly
+    dinos_eat_ferns
+    hungry_dinos_die
+    hatch_eggs
+    poop_eggs
+    decay_ferns
+    check_for_diseased_ferns
+    spawn_ferns
+
+    if @epoch % 5 == 0 
+      adjust_fern_spawnrate
+    end
+
+    @epoch += 1
+    
+    render json: { game_state: @game_state }
   }
 
-  def initialize_units 
+  def create_unit(name, x, y)
+    case name
+    when :dino
+      create_dino(x, y)
+    when :fern
+      create_fern(x, y)
+    when :diseased_fern
+      create_diseased_fern(x, y)
+    when :egg
+      create_egg(x, y)
+    end
+  end
 
   def create_dino(x, y)
-    @game_state[:dinos].push({ x: x, y: y, health: DINO_HEALTH })
+    @game_state[:dinos].push({ x: x, y: y, health: @dino_health })
   end
 
   def create_baby_dino(x, y)
-    @game_state[:dinos].push({ x: x, y: y, health: DINO_BABY_HEALTH })
+    @game_state[:dinos].push({ x: x, y: y, health: @dino_baby_health })
   end
 
   def create_fern(x, y)
-    @game_state[:ferns].push({ x: x, y: y, health: FERN_HEALTH })
+    @game_state[:ferns].push({ x: x, y: y, health: @fern_health })
   end
 
   def create_diseased_fern(x, y)
@@ -88,7 +105,174 @@ class GameController < ApplicationController
   end
 
   def create_egg(x, y)
-    @game_state[:eggs].push({ x: x, y: y})
+    @game_state[:eggs].push({ x: x, y: y, health: @egg_health })
+  end
+
+  def move_dino_randomly(dino)
+    directions = []
+    if dino[:y] < (@board_height - 1)
+      directions << :up
+    end
+    if dino[:y] > 0
+      directions << :down
+    end
+    if dino[:x] < (@board_width - 1)
+      directions << :right
+    end
+    if dino[:x] > 0
+      directions << :left
+    end
+    direction = directions.sample
+  
+    case direction
+    when :up
+      dino[:y] += 1
+    when :down
+      dino[:y] -= 1
+    when :left
+      dino[:x] -= 1
+    when :right
+      dino[:x] += 1
+    end
+  end
+
+  def move_dinos_randomly
+    @game_state[:dinos].each do |dino|
+      move_dino_randomly(dino)
+    end
+  end
+
+  def check_end_of_game
+    @game_state[:dinos].empty? && @game_state[:ferns].empty? && @game_state[:eggs].empty?
+  end
+
+  def dinos_eat_ferns
+    ferns_to_delete = []
+    dinos_to_delete = []
+    diseased_ferns_to_delete = []
+    @game_state[:dinos].each_with_index do |dino, dinoIndex|
+      # Eating health ferns
+      @game_state[:ferns].each_with_index do |fern, index|
+        if fern[:x] == dino[:x] && fern[:y] == dino[:y]
+          ferns_to_delete << index
+        end
+      end
+   
+      # Eating diseased ferns
+      @game_state[:diseased_ferns].each_with_index do |fern, index|
+        if fern[:x] == dino[:x] && fern[:y] == dino[:y]
+          diseased_ferns_to_delete << index
+          if (dino[:health] < 3)
+            dinos_to_delete << index
+          else
+            dino[:health] -= 2
+          end
+        end
+      end
+    end
+
+    ferns_to_delete.reverse_each { |index| @game_state[:ferns].delete_at(index) }
+    dinos_to_delete.reverse_each { |index| @game_state[:dinos].delete_at(index) }
+    diseased_ferns_to_delete.reverse_each { |index| @game_state[:diseased_ferns].delete_at(index) }
+  end
+
+  def hungry_dinos_die
+    dinos_to_delete = []
+    @game_state[:dinos].each_with_index do |dino, index|
+      if (dino[:health] <= 0)
+        dinos_to_delete << index
+      end
+    end
+    dinos_to_delete.reverse_each { |index| @game_state[:dinos].delete_at(index) }
+  end
+
+  def hatch_eggs
+    eggs_to_delete = []
+    @game_state[:eggs].each_with_index do |egg, index|
+      if egg[:health] > 0
+        egg[:health] -= 1
+      else
+        eggs_to_delete << index
+        create_unit(:dino, egg[:x], egg[:y])
+      end
+    end
+    eggs_to_delete.reverse_each { |index| @game_state[:eggs].delete_at(index) }
+        
+
+  def poop_eggs
+    @game_state[:dinos].combination(2).each do |dino1, dino2|
+      # If there's 2 healthy dinos in the same spot
+      if dino1[:x] == dino2[:x] && dino1[:y] == dino2[:y] && dino1[:health] > @dino_reproduce_health && dino2[:health] > @dino_reproduce_health
+        # But not an egg already
+        unless @game_state[:eggs].any? { |egg| egg[:x] == dino1[:x] && egg[:y] == dino1[:y] }
+          # make a new egg
+          create_unit(:egg, dino1[:x], dino1[:y])
+        end
+      end
+    end
+  end
+
+  def decay_ferns
+    ferns_to_delete = []
+    @game_state[:ferns].each_with_index do |fern, index|
+      if fern[:health] > 0
+        fern[:health] -= 1
+      else
+        ferns_to_delete << index
+      end
+    end
+    ferns_to_delete.reverse_each { |index| @game_state[:ferns].delete_at(index) }
+  end
+
+  def check_for_diseased_ferns
+    for i in 1..(@board_width - 2)
+      for j in 1..(@board_height - 2)
+        if !check_coordinate_for_empty_fern_spots(i, j)
+          make_coordinate_diseased_ferns(i, j)
+        end
+      end
+    end
+  end
+
+  def check_coordinate_for_empty_fern_spots(i, j)
+    has_empty = false
+    for ii in (i - 1)..(i + 1)
+      for jj in (j - 1)..(j + 1)
+        if !@game_state[:ferns].any? { |fern| fern[:x] == ii && fern[:y] == jj }
+          has_empty = true
+        end
+      end
+    end
+    has_empty 
+  end
+
+  def make_coordinate_diseased_ferns(i, j)
+    for ii in (i - 1)..(i + 1)
+      for jj in (j - 1)..(j + 1)
+        @game_state[:ferns].delete_if { |fern| fern[:x] == ii && fern[:y] == jj }
+        create_unit(:diseased_fern, ii, jj)
+      end
+    end
+  end
+
+  def spawn_ferns
+    for i in 0..@board_width
+      for j in 0..@board_height
+        if check_empty(i, j) && rand < @fern_spawnrate
+          create_unit(:fern, i, j)
+        end
+      end
+    end
+  end
+
+  def check_empty(i, j)
+    @game_state.any do |_, array|
+      array.any? { |el| el[:x] == i && el[:y] == j }
+    end
+  end
+
+  def adjust_fern_spawnrate
+    @fern_spawnrate = @game_state[:ferns].length / (@board_width * @board_height)
   end
   
 end
