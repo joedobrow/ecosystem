335cb931127911957436e26bc26a725bb0a98cb3
Joe Dobrow <joedobrow@MacBook-Pro.local>
2023-11-20 11:24:10 -0600

    Update Redis environment variable for session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,7 +2,7 @@
 Rails.application.config.session_store :redis_store, {
   servers: [
     {
-      url: ENV['REDIS_URL'],
+      url: ENV['redis://default:hmsl1ZzFx8Ac9tqjZSPbvmvsodfgOehM@redis-16905.c246.us-east-1-4.ec2.cloud.redislabs.com:16905'],
       namespace: "session"
     },
   ],
