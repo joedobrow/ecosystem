54f64026d7dbda394e1bd5efcadc60df25044e0b
Joe Dobrow <joedobrow@MacBook-Pro.local>
2023-11-20 14:14:44 -0600

    development redis setup

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,12 +1,16 @@
 # config/initializers/session_store.rb
-Rails.application.config.session_store :redis_store,
-  servers: [
-    {
-      url: ENV['REDISCLOUD_URL'],
-      namespace: "session"
-    },
-  ],
-  expire_after: 120.minutes,
-  key: "_ecosystem_game_session",
-  threadsafe: false,
-  secure: true
+if Rails.env.production?
+  Rails.application.config.session_store :redis_store,
+    servers: [
+      {
+        url: ENV['REDISCLOUD_URL'],
+        namespace: "session"
+      },
+    ],
+    expire_after: 120.minutes,
+    key: "_ecosystem_game_session",
+    threadsafe: false,
+    secure: true
+else 
+  Rails.application.config.session_store :cookie_store, key: '_ecosystem_game_session'
+end
