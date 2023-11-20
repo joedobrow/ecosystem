8a318c677fc970e531003ea89c78931bff50035a
Joe Dobrow <joedobrow@MacBook-Pro.local>
2023-11-20 17:22:17 -0600

    about page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get 'home/index', to: 'home#index'
   get 'home', to: 'home#index'
 
+  get 'about', to: 'about#index'
+
   get '/', to: 'home#index'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
